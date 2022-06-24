f08412647025cde22bfc97803fda45f9a2ae3d8e
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-06-24 11:51:59 +0200

    use single responsablity

--- Eval_CyrilGoldenschue/src/recipientsVisitor.rb
+++ Eval_CyrilGoldenschue/src/recipientsVisitor.rb
@@ -7,15 +7,25 @@ class RecipientsVisitor
 
     def visit
         if @config['recipients_filename']
-          return @recipients.concat(File.readlines(@config['recipients_filename'], chomp: true))
+          return self.file
         end
         
         if @config['recipients_dbconnection']
-          require "sqlite3"
-          db = SQLite3::Database.new "users.db"
-          db.execute("SELECT email FROM users") do |row|
-            return @recipients << row['email']
-          end
+          return self.database
         end
+    end
+
+    protected 
+    def file 
+      return @recipients.concat(File.readlines(@config['recipients_filename'], chomp: true))
+    end
+
+    def database
+      require "sqlite3"
+      db = SQLite3::Database.new "users.db"
+      db.execute("SELECT email FROM users") do |row|
+        return @recipients << row['email']
+      end
+
     end
 end
